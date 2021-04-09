abdd1abbdd1fe8e626b1cfe6d94dd68d53fc5e81
Michael Crawford <mbcrawfo@gmail.com>
2021-04-09 00:01:50 -0400

    Add Matrix3

--- source/RayTracer.Core/Math/Matrix3.cs
+++ source/RayTracer.Core/Math/Matrix3.cs
@@ -2,16 +2,16 @@ using System;
 
 namespace RayTracer.Core.Math
 {
-    public sealed class Matrix2 : Matrix
+    public sealed class Matrix3 : Matrix
     {
-        private const int Size = 2;
+        private const int Size = 3;
 
-        public Matrix2()
+        public Matrix3()
             : base(Size, Size)
         {
         }
 
-        public Matrix2(float[,] elements)
+        public Matrix3(float[,] elements)
             : base(elements)
         {
             if (elements.GetLength(0) != Size || elements.GetLength(1) != Size)
@@ -23,7 +23,7 @@ namespace RayTracer.Core.Math
             }
         }
 
-        public Matrix2(Matrix2 other)
+        public Matrix3(Matrix3 other)
             : base(other.Elements)
         {
         }
---
+++ tests/RayTracer.Core.UnitTests/Math/Matrix3Tests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using RayTracer.Core.Math;
+using Xunit;
+
+namespace RayTracer.Core.UnitTests.Math
+{
+    public class Matrix3Tests
+    {
+        public static IEnumerable<object> ArraysThatAreNot3X3 =>
+            new object[]
+            {
+                new object[] { new float[2, 2] },
+                new object[] { new float[2, 3] },
+                new object[] { new float[3, 2] },
+                new object[] { new float[4, 4] },
+            };
+
+        [Theory]
+        [MemberData(nameof(ArraysThatAreNot3X3))]
+        public void Constructor__Array_ShouldThrowArgumentException_WhenElementsArrayIsNot3x3(
+            float[,] array
+        )
+        {
+            // arrange
+            // act
+            Action act = () => { _ = new Matrix3(array); };
+
+            // assert
+            act.Should().Throw<ArgumentException>().WithMessage("*must be a 3x3 array*");
+        }
+
+        [Fact]
+        public void Constructor__Array_ShouldCreate3x3MatrixAndAssignElementsFromTheArray()
+        {
+            // arrange
+            var array = new[,] { { 1f, 2f, 3f }, { 4f, 5f, 6f }, { 7f, 8f, 9f } };
+
+            // act
+            var result = new Matrix3(array);
+
+            // assert
+            using var _ = new AssertionScope();
+            result.Rows.Should().Be(3);
+            result.Columns.Should().Be(3);
+            result[0, 0].Should().Be(1f);
+            result[0, 1].Should().Be(2f);
+            result[0, 2].Should().Be(3f);
+            result[1, 0].Should().Be(4f);
+            result[1, 1].Should().Be(5f);
+            result[1, 2].Should().Be(6f);
+            result[2, 0].Should().Be(7f);
+            result[2, 1].Should().Be(8f);
+            result[2, 2].Should().Be(9f);
+        }
+
+        [Fact]
+        public void Constructor__Copy_ShouldCreate3x3MatrixAndAssignElementsFromTheOtherMatrix()
+        {
+            // arrange
+            var other = new Matrix3(new[,] { { 1f, 2f, 3f }, { 4f, 5f, 6f }, { 7f, 8f, 9f } });
+
+            // act
+            var result = new Matrix3(other);
+
+            // assert
+            using var _ = new AssertionScope();
+            result.Rows.Should().Be(3);
+            result.Columns.Should().Be(3);
+            result[0, 0].Should().Be(1f);
+            result[0, 1].Should().Be(2f);
+            result[0, 2].Should().Be(3f);
+            result[1, 0].Should().Be(4f);
+            result[1, 1].Should().Be(5f);
+            result[1, 2].Should().Be(6f);
+            result[2, 0].Should().Be(7f);
+            result[2, 1].Should().Be(8f);
+            result[2, 2].Should().Be(9f);
+        }
+
+        [Fact]
+        public void Constructor__Default_ShouldCreate3x3Matrix()
+        {
+            // arrange
+            // act
+            var result = new Matrix3();
+
+            // assert
+            using var _ = new AssertionScope();
+            result.Rows.Should().Be(3);
+            result.Columns.Should().Be(3);
+        }
+    }
+}
