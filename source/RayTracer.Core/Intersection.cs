a7939a97e7a89f5eadcd873e6dd481f2f4d98acf
Michael Crawford <mbcrawfo@gmail.com>
2021-04-19 17:08:01 -0400

    Intersection.TimeComparer to static method

--- source/RayTracer.Core/Intersection.cs
+++ source/RayTracer.Core/Intersection.cs
@@ -1,13 +1,9 @@
-using System.Collections.Generic;
-using System.Diagnostics;
 using RayTracer.Core.Shapes;
 
 namespace RayTracer.Core
 {
     public sealed record Intersection(Shape Shape, float Time)
     {
-        public static readonly IComparer<Intersection> TimeComparer = new TimeComparerImpl();
-
         public IntersectionComputations PrepareComputations(Ray ray)
         {
             var point = ray.Position(Time);
@@ -24,15 +20,6 @@ namespace RayTracer.Core
             return new IntersectionComputations(Shape, Time, point, eye, normal, inside);
         }
 
-        private sealed class TimeComparerImpl : IComparer<Intersection>
-        {
-            /// <inheritdoc />
-            public int Compare(Intersection? x, Intersection? y)
-            {
-                Debug.Assert(x != null, nameof(x) + " != null");
-                Debug.Assert(y != null, nameof(y) + " != null");
-                return x.Time.CompareTo(y.Time);
-            }
-        }
+        public static int TimeComparer(Intersection a, Intersection b) => a.Time.CompareTo(b.Time);
     }
 }
