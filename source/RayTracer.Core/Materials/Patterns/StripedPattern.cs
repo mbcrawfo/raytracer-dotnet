06910ad90a68c3121b14a8e00f01e5000288bb18
Michael Crawford <mbcrawfo@gmail.com>
2021-05-29 18:58:16 -0400

    Added striped color patterns

---
+++ source/RayTracer.Core/Materials/Patterns/StripedPattern.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using RayTracer.Core.Math;
+
+namespace RayTracer.Core.Materials.Patterns
+{
+    public record StripedPattern : Pattern
+    {
+        public StripedPattern()
+        {
+        }
+
+        public StripedPattern(params Color[] colors)
+        {
+            Colors = colors.ToImmutableArray();
+        }
+
+        public IImmutableList<Color> Colors { get; init; } = ImmutableArray<Color>.Empty;
+
+        /// <inheritdoc />
+        public override Color ColorAt(in Point localPoint)
+        {
+            Debug.Assert(Colors.Count > 0, "Colors.Count > 0");
+
+            var index = (int) MathF.Abs(MathF.Floor(localPoint.X)) % Colors.Count;
+            return Colors[index];
+        }
+    }
+}
---
+++ tests/RayTracer.Core.UnitTests/Materials/Patterns/StripedPatternTests.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using RayTracer.Core.Materials.Patterns;
+using RayTracer.Core.Math;
+using Xunit;
+
+namespace RayTracer.Core.UnitTests.Materials.Patterns
+{
+    public class StripedPatternTests
+    {
+        public static IEnumerable<object> ColorAtTestCases =>
+            new[]
+            {
+                new object[] { -2f, Color.Blue },
+                new object[] { -1.1f, Color.Blue },
+                new object[] { -1f, Color.White },
+                new object[] { -0.1f, Color.White },
+                new object[] { 0f, Color.Red },
+                new object[] { 0.9f, Color.Red },
+                new object[] { 1f, Color.White },
+                new object[] { 2f, Color.Blue },
+                new object[] { 3f, Color.Red },
+                new object[] { 4f, Color.White },
+                new object[] { 5f, Color.Blue },
+            };
+
+        [Theory]
+        [MemberData(nameof(ColorAtTestCases))]
+        public void ColorAt_ShouldAlternateColors_WhenMovingAlongTheXAxis(
+            float x,
+            in Color expected
+        )
+        {
+            // arrange
+            var point = new Point(x, 0f, 0f);
+            var sut = new StripedPattern(Color.Red, Color.White, Color.Blue);
+
+            // act
+            var actual = sut.ColorAt(point);
+
+            // assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0f)]
+        [InlineData(1f)]
+        [InlineData(2f)]
+        public void ColorAt_ShouldNotChangeColor_WhenMovingAlongTheYAxis(float y)
+        {
+            // arrange
+            var point = new Point(0f, y, 0f);
+            var sut = new StripedPattern(Color.White, Color.Black);
+
+            // act
+            var result = sut.ColorAt(point);
+
+            // assert
+            result.Should().Be(Color.White);
+        }
+
+        [Theory]
+        [InlineData(0f)]
+        [InlineData(1f)]
+        [InlineData(2f)]
+        public void ColorAt_ShouldNotChangeColor_WhenMovingAlongTheZAxis(float z)
+        {
+            // arrange
+            var point = new Point(0f, 0f, z);
+            var sut = new StripedPattern(Color.White, Color.Black);
+
+            // act
+            var result = sut.ColorAt(point);
+
+            // assert
+            result.Should().Be(Color.White);
+        }
+    }
+}
